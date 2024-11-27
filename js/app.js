51ad812a40717479724b9723314295d59a27b8b0
luckyseo <ityun777@gmail.com>
2024-11-27 15:41:34 +1100

    the clock is ticking!

--- js/app.js
+++ js/app.js
@@ -1,12 +1,9 @@
-const channelBar = document.querySelector("#toggle-arrow")
-const channelContent = document.querySelector("#channel-content")
 
-function toggleChannel(event){
-    if(channelBar.classList.contains("fa-chevron-up")){
-    channelBar.classList.replace("fa-chevron-up","fa-chevron-down");}
-    else{
-        channelBar.classList.replace("fa-chevron-down","fa-chevron-up");
-    }
+const clock = document.querySelector("#clock")
+
+function getClock(){
+    const date = new Date()
+    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
 }
-
-channelBar.addEventListener("click", toggleChannel)
+getClock()
+setInterval(getClock,1000)
